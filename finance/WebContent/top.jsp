8383b684b884bf54f5c90969dcee9c65ee5dcbd7
User <User@HayleyCom>
2016-05-16 00:33:14 +0900

    Backup 05/16 12:30am

--- finance/WebContent/top.jsp
+++ finance/WebContent/top.jsp
@@ -17,7 +17,7 @@
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->       
-    <link rel="shortcut icon" href="/finance//images/ico/favicon.ico">
+    <link rel="shortcut icon" href="/finance/images/ico/favicon.ico">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/finance/images/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/finance/images/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/finance/images/ico/apple-touch-icon-72-precomposed.png">
@@ -44,7 +44,7 @@
                         <li class="active"><a href="#">Home</a></li>          
                         <li><a href="/finance/analysis/analysis_main.html">투자분석</a></li>
                         <li><a href="/finance/product/product_main.html">금융상품</a></li>
-                        <li><a href="/finance/portfolio/">포트폴리오</a></li>
+                        <li><a href="/finance/portfolio/portfolio_survey.html">포트폴리오</a></li>
                      	
                     </ul>
                 </div>
--- finance/src/portfolio/controller/PortfolioServlet.java
+++ finance/src/portfolio/controller/PortfolioServlet.java
@@ -1,6 +1,10 @@
 package portfolio.controller;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
@@ -8,11 +12,52 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-@WebServlet(name = "survey", urlPatterns = { "/survey.do" })
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@WebServlet(name = "portfolio", urlPatterns = { "/portfolio.do" })
 public class PortfolioServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		res.setContentType("Application/json; charset=utf-8");
+		
+		JSONParser json_parser = new JSONParser();
+		JSONObject jsonobj = null;
+		try {
+			jsonobj = (JSONObject)json_parser.parse("objSurveyData");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		String param01 = (String)jsonobj.get("objSurveyData");
+	
+		
+		JSONArray pramlist = (JSONArray)jsonobj.get("objSurveyData");
+		
+		for(int i =0; i < pramlist.size(); i++){
+			String arrayData = (String)pramlist.get(i);
+			System.out.println("list : " + arrayData);
+		}
 
+		
+/*		BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+		String json = "";
+		if(br != null){
+			json = br.readLine();
+			
+		}
+			ObjectMapper mapper = new ObjectMapper();
+
+			*/
+				
+		
 		
 		
 	}
