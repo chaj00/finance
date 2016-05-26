ea67a1c53206d7f9f96dc3ae410be0cc0e79f18b
kitri <kitri@DESKTOP-V3BRE1G>
2016-05-26 17:49:09 +0900

    ee

---
+++ finance/src/portfolio/controller/ParamSetting.java
@@ -0,0 +1,24 @@
+package portfolio.controller;
+
+import java.util.ArrayList;
+
+import portfolio.dto.PortfolioDTO;
+import portfolio.service.PortfolioService;
+import portfolio.service.PortfolioServiceImpl;
+
+public class ParamSetting {
+	public ArrayList<PortfolioDTO> ParamSetting(String type, String std, String rownum){
+		PortfolioService service = new PortfolioServiceImpl();
+		String str_type = type;
+		String str_std = std;
+		String str_rownum = rownum;
+		ArrayList<PortfolioDTO> pf_stock_list = service.getPlist(str_type, str_std, str_rownum);
+		
+		return pf_stock_list;
+		
+	}
+	
+	public String removeNum(String str) {
+	    return str.replaceAll("[^0-9]", "");
+	}
+}
